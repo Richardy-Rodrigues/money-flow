8950572f5c4423e3000930564438c2c459fa1843
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-08-23 14:24:59 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,33 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { ArrowDown, ArrowUp } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="text-primary rounded-full bg-green-800 px-2 py-1 font-bold hover:bg-green-800">
+        <ArrowUp className="fill-primary mr-1" />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="text-primary rounded-full bg-red-800 px-2 py-1 font-bold hover:bg-red-800">
+        <ArrowDown className="fill-primary mr-1" />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="text-primary rounded-full bg-gray-800 px-2 py-1 font-bold hover:bg-gray-800">
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,28 @@
-const TransactionsPage = () => {
-    return <h1>TransactionsPage Page</h1>;
-}
- 
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TITLE AND BUTTON */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="cursor-pointer rounded-full bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-600">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      {/* TRANSACTION TABLE */}
+      <div className="">
+        <DataTable columns={TransactionColumns} data={transactions} />
+      </div>
+      {/* PAGINATION */}
+    </div>
+  );
+};
+
 export default TransactionsPage;
